906876eca2cae4dba8f1c27a04f72dc633ce2391
Anıl Özfırat <anilozfirat@gmail.com>
2026-01-18 01:09:52 +0300

    test

--- src/lib/supabaseClient.ts
+++ src/lib/supabaseClient.ts
@@ -1,7 +1,7 @@
+// Supabase Client Configuration
 import { createClient } from '@supabase/supabase-js';
 
 // Vercel ayarlarıyla uğraşmamak için anahtarları buraya sabitliyoruz.
-// Bu anahtarlar "anon" (public) olduğu için tarayıcıda görünmesinde teknik bir sakınca yoktur.
 const supabaseUrl = "https://pfutcxhomkmuejpfhkza.supabase.co";
 const supabaseKey = "sb_publishable_e1-3KXpEy_ey70qMcuALiA_uUpkEW2V"; // Sizin verdiğiniz key
 
